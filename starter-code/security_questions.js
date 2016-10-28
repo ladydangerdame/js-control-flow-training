64f65b50f02d1b29343ee96f41ef40c332f5bfa6
ladydangerdame <turbofemme@gmail.com>
2016-10-27 22:23:57 -0700

    Wrote questions and answers attempted further code

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,32 @@
 console.log("security_questions.js loaded");
+
+var securityQuestions = [
+   {
+     question: "What was your first pet's name?",
+     expectedAnswer: "FlufferNutter"
+  },
+    {
+      question: "What is your mother's maiden name?"
+      expectedAnswer: "Davenport"
+  },
+    {
+      question: "What street did you grow up on?"
+      expectedAnswer: "Washington"
+  }
+]
+
+var userAnswer = "";
+var notUser = false;
+
+for (var i=0; i < securityQuestions.length; i++){
+	userAnswer = prompt(securityQuestions[i].question);
+	if (userAnswer !== securityQuestions[i].expectedAnswer){
+		alert("Wrong!");
+    notUser = true;
+		break;
+	}
+}
+
+if (notUser){
+  console.log("Locked!");
+}
